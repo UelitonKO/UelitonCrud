264b6bf65f4a37cee91f0fa89891d693060fb9b3
2022103030030 <2022103030030@VHA-LAB3-34>
2023-11-28 10:15:21 -0400

    opa

--- UelitonCrud/DAL/Conexao.cs
+++ UelitonCrud/DAL/Conexao.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace UelitonCrud.DAL
 {
     internal class Conexao
     {
+        //propriedades para conectar ao banco de dados
+        string conecta = "SERVER=localhost; DATABASE=pessoas; UID=root; PWD=SUPORTE99";
+
+        protected MySqlConnection conexao = null;
+
+        //metodo para conectar ao banco de dados
+        public void AbrirConexao()
+        {
+            try
+            {
+                conexao = new MySqlConnection(conecta);
+                conexao.Open();
+            }
+            catch (Exception erro)
+            {
+
+                throw erro;
+            }
+        }
+        // metodo para fechar a conexão
+
+        public void FecharConexao()
+        {
+            try
+            {
+                conexao = new MySqlConnection(conecta);
+                conexao.Close(); 
+            }
+            catch (Exception erro)
+            {
+
+                throw erro;
+            }
+        }
+
+
+
     }
 }
